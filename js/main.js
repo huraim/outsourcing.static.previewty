28ba8da374d3e641806db385bae274dabf19fd53
taka <taka@mewpot.com>
2022-06-18 11:47:02 +0900

    [UI] previewty_cover First Section PC 완성

--- js/main.js
+++ js/main.js
@@ -1,35 +1,8 @@
-$(function(){
+window.addEventListener('load', function(){
+    console.log('ok');
 
-  var divs =null;
 
-  var test = document.querySelector('#test');
 
-  console.log( test.classList.item(0) );
 
-$('.photos-board-item').each(function(i){
-    $(this).data('initial-index', i);
-});
-
-$('.hide-others').on('click', function () {
-    if(divs) {
-        $(divs).appendTo('.row').each(function(){
-            var oldIndex = $(this).data('initial-index');
-            $('.photos-board-item').eq(oldIndex).before(this);
-            console.log(oldIndex);
-        });
-        
-        divs = null;
-    } else {
-        divs = $('.css--all-photo').detach();
-        console.log(divs);
-    }
-});
-
-    const num1 = '402';
-    const num2 = '500';
-
-    if(num1 == 400 | 500 | 501 | 502) {
-        console.log(num1)
-    }
 
 });
