69bfa28be37016523430a4bd035b01f2a0825248
taka <taka@mewpot.com>
2022-06-19 19:03:41 +0900

    [Making] previewty_package Slide 부분 임시 작업 중

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 window.addEventListener('load', function(){
     console.log('ok');
 
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2000,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+    });
+
 
 
 
