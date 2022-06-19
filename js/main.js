5910735f97b2a8f0d27753b1868dd8b361eae30c
taka <taka@mewpot.com>
2022-06-19 23:33:56 +0900

    [UI] previewty_package Seciont 완성

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
-window.addEventListener('load', function(){
-    console.log('ok');
+window.addEventListener('load', () => {
 
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 4,
-        spaceBetween: 24,
-        centeredSlides: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        autoplay: {
-            delay: 2000,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-    });
+    const nextBtn = document.querySelector('.previewty_package_slide_next');
+    const prevBtn = document.querySelector('.previewty_package_slide_prev');
+    const packageSlide = document.querySelector('.previewty_package_slide');
+    let active_num = 1;
 
+    window.addEventListener("resize", resizeFunction);
+    resizeFunction();
 
+    function resizeFunction() {
+        winW = window.innerWidth;
+        if(winW > 991) {
+            packageSlide.prepend(packageSlide.lastElementChild);
+            packageSlide.prepend(packageSlide.lastElementChild);
+        
+            nextBtn.addEventListener('click', () => {
+                packageSlide.prepend(packageSlide.lastElementChild);
+                packageSlide.prepend(packageSlide.lastElementChild);
+                if(active_num >= packageSlide.children.length) {
+                    active_num = 1;
+                }
+                else {
+                    active_num += 1;
+                }
+                for(var i=0; i<packageSlide.children.length; i++) {
+                    console.log(packageSlide.children[i]);
+                    packageSlide.children[i].classList.remove('active');
+                }
+                document.querySelector(`#previewty_package_${active_num}`).classList.add('active')
+            });
+            prevBtn.addEventListener('click', () => {
+                packageSlide.append(packageSlide.firstElementChild);
+                packageSlide.append(packageSlide.firstElementChild);
+                if(active_num <= 1) {
+                    active_num = 6;
+                }
+                else {
+                    active_num -= 1;
+                }
+                for(var i=0; i<packageSlide.children.length; i++) {
+                    console.log(packageSlide.children[i]);
+                    packageSlide.children[i].classList.remove('active');
+                }
+                document.querySelector(`#previewty_package_${active_num}`).classList.add('active')
+            });
+        
+        }
+    }
 
 
 
